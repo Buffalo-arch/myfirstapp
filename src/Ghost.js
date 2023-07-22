e42dcafac6342b51a6aac306002b259620fbd0f8
Buffalo-arch <118945895+Buffalo-arch@users.noreply.github.com>
2023-07-22 09:17:09 +0545

    first commit

---
+++ src/Ghost.js
@@ -0,0 +1,5 @@
+function Ghost() {
+  return <h1>It is anonymous.</h1>;
+}
+
+export default Ghost;
---
+++ src/Witch.js
@@ -0,0 +1,4 @@
+function Witch() {
+  return <h1> Who flies high on the sky.</h1>;
+}
+export default Witch;
